af7a5845f60a3e17b06b2bedd0a28f03daff4f75
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2020-12-01 19:55:44 +0100

    Return early

--- 1-1/1-1.py
+++ 1-1/1-1.py
@@ -5,10 +5,14 @@ def read_integers(filename):
     with open(filename) as f:
         return [int(x) for x in f]
 
-goal = 2020
-ints = read_integers("input.txt")
-for first in ints:
-    for second in ints:
-        result = first + second
-        if result == 2020:
-            print(first*second)
+def horse():
+    goal = 2020
+    ints = read_integers("input.txt")
+    for first in ints:
+        for second in ints:
+            result = first + second
+            if result == 2020:
+                print(first*second)
+                return
+
+horse()
