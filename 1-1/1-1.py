702ac5cffab8ef0e6fef0308f511d308c092c046
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2020-12-01 19:32:05 +0100

    Try to make interpretable Python

--- 1-1/1-1.py
+++ 1-1/1-1.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env /usr/bin/python
+
 # Thanks to Rudolph: https://codereview.stackexchange.com/a/12449
 def read_integers(filename):
     with open(filename) as f:
         return [int(x) for x in f]
+
+read_integers("input.txt")
