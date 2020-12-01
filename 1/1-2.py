19a9b6a98138f46b623ca57797dd2d8510ed3a84
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2020-12-01 19:59:01 +0100

    1-2

---
+++ 1/1-2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env /usr/bin/python
+
+# Copied over from 1-1.py
+# ---
+# Thanks to Rudolph: https://codereview.stackexchange.com/a/12449
+def read_integers(filename):
+    with open(filename) as f:
+        return [int(x) for x in f]
+
+def horse():
+    goal = 2020
+    ints = read_integers("input.txt")
+    for first in ints:
+        for second in ints:
+            for third in ints:
+                result = first + second + third
+                if result == 2020:
+                    print(first*second*third)
+                    return
+
+horse()
