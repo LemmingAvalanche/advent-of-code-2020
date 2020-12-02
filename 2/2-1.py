3d494f186f46e2aa1ea36b34c395551bb9123c4c
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2020-12-02 11:59:58 +0100

    2-1: first attempt, doesn’t work

--- 2/2-1.py
+++ 2/2-1.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
+from itertools import compress
+
 def parse(filename):
     with open(filename) as f:
-        for x in f:
-            parse_line(x)
+        # Ok, this is not great
+        print(sum(bool(y) for y in [check_psw(x) for x in f]))
 
-
-def parse_line(line):
+def check_psw(line):
     split_on_ws = line.split(" ");
     range = parse_range(split_on_ws[0])
+    at_least = int(range[0])
+    at_most = int(range[1])
     ch = parse_char(split_on_ws[1])
     psw = split_on_ws[2]
-    print(range)
-    print(ch)
-    print(psw)
+    count = psw.count(psw)
+    if count >= at_least and count <= at_most:
+        return True
+    else:
+        return False
 
 def parse_range(range):
     r = range.split("-")
