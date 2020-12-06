08e29e191c337eaab45512d668f786adb6cc350a
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2020-12-06 13:20:54 +0100

    main: add note

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 mod day6;
 
 fn main() {
+    // I just update this function for each problem that I bother to solve,
+    // setting the input file path and the function call.
     let input = fs::read_to_string(
         "<path to input>")
         .unwrap();
